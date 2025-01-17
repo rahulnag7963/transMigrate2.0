7a1a134d3842d2be0c02af51f65c1a1d65265c89
rahulnag7963 <rahulnag7963@gmail.com>
2025-01-16 22:11:22 -0700

    Updated website desc

--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,15 @@ export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   name: "transMigrate",
-  description: "Creating beutiful websites together.",
+  description: `Transmigrate: Transforming Ideas into Stunning Websites
+
+Welcome to Transmigrate, where innovation meets web design! Our mission is to turn your digital dreams into reality with sleek, modern, and user-focused websites that captivate and engage.
+
+Whether you're a startup aiming to make a bold first impression, a business looking to expand its digital presence, or a visionary with a unique idea, Transmigrate is here to bring your concepts to life.
+
+With cutting-edge technologies, seamless functionality, and designs tailored to your brand, we create websites that don't just look good—they work brilliantly.
+
+Let’s transmigrate your vision into the digital world. Start your journey with us today!`,
   navItems: [
     {
       label: "Home",
